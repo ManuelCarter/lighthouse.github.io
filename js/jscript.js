33358ba9a2f4775b91279b40c6b3a1cd4090f4be
ManuelCarter <itzlumide@gmail.com>
2019-03-29 20:50:43 +0100

    Lighthouse motivation

---
+++ js/jscript.js
@@ -0,0 +1,26 @@
+/*
+var liteColor = document.getElementById("lite");
+      var liteColor2 = document.getElementById("lite2");
+      var liteColor0 = document.getElementById("lite0");
+      var liteColor9 = document.getElementById("lite9");
+      var nlite = ["liteColor2","liteColor0","liteColor9"];
+      var nlite0 = ["liteColor","liteColor2","liteColor9"];
+      var nlite2 = ["liteColor","liteColor0","liteColor9"];
+      var nlite9 = ["liteColor","liteColor2","liteColor0"];
+function lite(){
+      liteColor.style.color="#e30a73";
+      nlite.style.color="#444";
+  }
+  function lite2(){
+      liteColor2.style.color="#e30a73";
+      nlite2.style.color="#444";
+  }
+    function lite0(){
+      liteColor0.style.color="#e30a73";
+      nlite0.style.color="#444";
+  }
+    function lite9(){
+      liteColor9.style.color="#e30a73";
+      nlite9.style.color="#444";
+  }
+  */
